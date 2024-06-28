c61203a379707cbf3dd64445eebef60008a70ebb
igarashi.aya <igarashi.aya@skyseer.co.jp>
2024-06-28 13:14:38 +0900

    fix roginpageurl

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -24,7 +24,7 @@ const Header = async () => {
             ホーム
           </Link>
           <Link
-            href={user ? "/profile" : "/app/auth/signin"}
+            href={user ? "/profile" : "/api/auth/signin"}
             className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
           >
             {user ? "プロフィール" : "ログイン"}
